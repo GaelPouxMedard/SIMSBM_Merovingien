10b0f8ebbc3946dd404fb94e88c170b19a1cac13
GaelPouxMedard <61467946+GaelPouxMedard@users.noreply.github.com>
2022-01-11 18:33:24 +0100

    Changes

--- Visualisations.py
+++ Visualisations.py
@@ -6,6 +6,7 @@ import itertools
 from sklearn import metrics
 from scipy.stats import sem
 import matplotlib.pyplot as plt
+from copy import copy
 import seaborn as sns
 
 if "Plots" not in os.listdir("."): os.mkdir("./Plots")
@@ -78,7 +79,7 @@ def findClusters(mat):
     return cluster.labels_
 
 def writeClusters(thetas, codeSave):
-    with open(f"Plots/Merovingien/{codeSave}_composition_clusters.txt", "w+", encoding="utf-8") as f:
+    with open(f"Plots/Merovingien/{codeSave}composition_clusters.txt", "w+", encoding="utf-8") as f:
         for num_clus, k in enumerate(thetas[0].T):
             sorted_incides = list(reversed(np.argsort(k)))
             f.write(f"Cluster {num_clus}\n")
@@ -96,26 +97,26 @@ def plotThetas(thetas, intToFeat, codeSave):
     plt.figure(figsize=(7, 9*len(thetas)))
 
     nbTypes = len(thetas)
-    for type in range(len(thetas)):
-        # maxTheta = np.max(thetas[type])
-        # thetas[type] = np.exp(-scaleAlpha * (maxTheta - thetas[type]))
+    for num_theta in range(len(thetas)):
+        # maxTheta = np.max(thetas[num_theta])
+        # thetas[num_theta] = np.exp(-scaleAlpha * (maxTheta - thetas[num_theta]))
 
-        shift = (type - nbTypes / 2) * scaleTypes
-        nbFeat = thetas[type].shape[0] - 1
-        nbClus = thetas[type].shape[-1] - 1
+        shift = (num_theta - nbTypes / 2) * scaleTypes
+        nbFeat = thetas[num_theta].shape[0] - 1
+        nbClus = thetas[num_theta].shape[-1] - 1
 
         clusLab = findClusters(thetas[0])
         pos = 0
         for clus in range(len(clusLab)):
             for i in np.where(clusLab == clus)[0]:
-                plt.text(-1, (pos - nbFeat / 2) * scaleFeat + shift, intToFeat[type][i], ha="right", va="center", fontsize=11)
-                for k in range(len(thetas[type][i])):
-                    plt.plot([-1, 0], [(pos - nbFeat / 2) * scaleFeat + shift, (k - nbClus / 2) * scaleClus + shift], "k-", linewidth=thetas[type][i][k], alpha=thetas[type][i][k])
+                plt.text(-1, (pos - nbFeat / 2) * scaleFeat + shift, intToFeat[num_theta][i], ha="right", va="center", fontsize=11)
+                for k in range(len(thetas[num_theta][i])):
+                    plt.plot([-1, 0], [(pos - nbFeat / 2) * scaleFeat + shift, (k - nbClus / 2) * scaleClus + shift], "k-", linewidth=thetas[num_theta][i][k], alpha=thetas[num_theta][i][k])
                 pos += 1
 
         plt.text(-1, (nbFeat*1.3 / 2) * scaleFeat + shift, "Inputs", ha="center", va="center", fontsize=12)
         plt.text(0, (nbClus*1.3 / 2)*scaleClus + shift, "Clusters", ha="center", va="center", fontsize=12)
-        for k in range(thetas[type].shape[1]):
+        for k in range(thetas[num_theta].shape[1]):
             plt.plot(0, (k - nbClus / 2) * scaleClus + shift, "or", markersize=25)
             plt.text(0, (k - nbClus / 2) * scaleClus + shift, str(k), ha="center", va="center", fontsize=12)
 
@@ -189,15 +190,148 @@ def plotGraph2D(thetas, p, intToOut, intToFeat, codeSave):
     plt.close()
 
 
-def plotRes():
+def readRes():
     folder = "Results/Merovingien/"
     files = os.listdir(folder)
-    "_[0]_[3]_[2]_[7]_0_Avg_Results"
 
+    metricsToExclude = ["CovErr", "CovErrNorm"]
+
+    dicAvg, dicStd = {}, {}
+    matAvg, matStd = [], []
     for file in files:
-        print(file)
+        if file[0]!="_": continue
+        if "Avg_Results" not in file: continue
+        
+        params = file.split("_")
+        fold, features, output, DS, nbInterp, nbClus = params[1:7]
 
-    savefig("Results")
+        if features not in dicAvg: dicAvg[features]={}
+        if output not in dicAvg: dicAvg[features][output]={}
+        if DS not in dicAvg[features][output]: dicAvg[features][output][DS]={}
+        if nbInterp not in dicAvg[features][output][DS]: dicAvg[features][output][DS][nbInterp]={}
+        if nbClus not in dicAvg[features][output][DS][nbInterp]: dicAvg[features][output][DS][nbInterp][nbClus]={}
+        if fold not in dicAvg[features][output][DS][nbInterp][nbClus]: dicAvg[features][output][DS][nbInterp][nbClus][fold]={}
+
+        if features not in dicStd: dicStd[features]={}
+        if output not in dicStd: dicStd[features][output]={}
+        if DS not in dicStd[features][output]: dicStd[features][output][DS]={}
+        if nbInterp not in dicStd[features][output][DS]: dicStd[features][output][DS][nbInterp]={}
+        if nbClus not in dicStd[features][output][DS][nbInterp]: dicStd[features][output][DS][nbInterp][nbClus]={}
+        if fold not in dicStd[features][output][DS][nbInterp][nbClus]: dicStd[features][output][DS][nbInterp][nbClus][fold]={}
+
+
+        with open(folder + file, 'r') as f:
+            labels = f.readline().replace("\n", "").split("\t")
+            results = f.readline().replace("\n", "").split("\t")
+
+            for lab, res in zip(labels[1:], results[1:]):
+                if res == '' or lab in metricsToExclude: continue
+                dicAvg[features][output][DS][nbInterp][nbClus][fold][lab] = float(res)
+                matAvg.append([fold, features, output, DS, nbInterp, nbClus, lab, float(res)])
+
+        with open(folder + file.replace("Avg", "Std"), 'r') as f:
+            labels = f.readline().replace("\n", "").split("\t")
+            results = f.readline().replace("\n", "").split("\t")
+
+            for lab, res in zip(labels[1:], results[1:]):
+                if res == '' or lab in metricsToExclude: continue
+                dicStd[features][output][DS][nbInterp][nbClus][fold][lab] = float(res)
+                matStd.append([fold, features, output, DS, nbInterp, nbClus, lab, float(res)])
+                
+
+    return np.array(matAvg, dtype=object), np.array(matStd, dtype=object)
+
+    
+def plotRes():
+    matAvg, matStd = readRes()
+
+    names = ["fold", "features", "output", "DS", "nbInterp", "nbClus"]
+    for whatToPlot in [[4, 5], [0], [4], [5]]:
+        dicToPlotx = {}
+        dicToPlotAvg = {}
+        dicToPlotStd = {}
+        for val, err in zip(matAvg, matStd):
+            valName = copy(val)
+            for wt in range(len(whatToPlot)):
+                if names[whatToPlot[wt]]=="fold": val[whatToPlot[wt]]=val[whatToPlot[wt]].split("of")[0]
+                valName[whatToPlot[wt]] = "All"
+
+            redKey = f"{valName[0]}_{valName[1]}_{valName[2]}_{valName[3]}_{valName[4]}_{valName[5]}_"
+
+            if redKey not in dicToPlotAvg:
+                dicToPlotx[redKey] = {}
+                dicToPlotAvg[redKey] = {}
+                dicToPlotStd[redKey] = {}
+            if val[-2] not in dicToPlotx[redKey]:
+                dicToPlotx[redKey][val[-2]] = []
+                dicToPlotAvg[redKey][val[-2]] = []
+                dicToPlotStd[redKey][val[-2]] = []
+
+            coord = tuple([float(val[whatToPlot[wt]].replace("]", "").replace("[", "")) for wt in range(len(whatToPlot))])
+            dicToPlotx[redKey][val[-2]].append(coord)
+            dicToPlotAvg[redKey][val[-2]].append(val[-1])
+            dicToPlotStd[redKey][val[-2]].append(err[-1])
+
+        if len(whatToPlot)==1:
+            for redKey in dicToPlotx:
+                for label in dicToPlotAvg[redKey]:
+                    dicToPlotx[redKey][label] = [v[0] for v in dicToPlotx[redKey][label]]
+                    dicToPlotAvg[redKey][label] = np.array([x for _, x in sorted(zip(dicToPlotx[redKey][label], dicToPlotAvg[redKey][label]))])
+                    dicToPlotStd[redKey][label] = np.array([x for _, x in sorted(zip(dicToPlotx[redKey][label], dicToPlotStd[redKey][label]))])
+                    dicToPlotx[redKey][label] = np.array([x for _, x in sorted(zip(dicToPlotx[redKey][label], dicToPlotx[redKey][label]))])
+
+                    plt.plot(dicToPlotx[redKey][label], dicToPlotAvg[redKey][label], label=label)
+                    plt.fill_between(dicToPlotx[redKey][label], dicToPlotAvg[redKey][label]-dicToPlotStd[redKey][label], dicToPlotAvg[redKey][label]+dicToPlotStd[redKey][label], alpha=0.3)
+
+                plt.title(str(list(zip(names, redKey.split("_")))))
+                plt.xlabel(names[whatToPlot[0]])
+                plt.ylabel("Metrics")
+                plt.legend()
+                plt.tight_layout()
+                nameFig = ""
+                if whatToPlot == [0]: nameFig = "Folds"
+                if whatToPlot == [4]: nameFig = "Interp"
+                if whatToPlot == [5]: nameFig = "Clusters"
+                savefig(nameFig, codeSave=redKey)
+                plt.close()
+
+        elif len(whatToPlot)==2:
+            for redKey in dicToPlotx:
+                label = "Acc"
+                if label not in dicToPlotAvg[redKey]: continue
+
+                arrx, arry = set(), set()
+                for x,y in dicToPlotx[redKey][label]:
+                    arrx.add(x)
+                    arry.add(y)
+
+                valToIndx = {v: i for i, v in enumerate(sorted(arrx))}
+                valToIndy = {v: i for i, v in enumerate(sorted(arry))}
+
+                valToIndx_inv = {i: v for v, i in valToIndx.items()}
+                valToIndy_inv = {i: v for v, i in valToIndy.items()}
+
+                coords = []
+                for x,y in dicToPlotx[redKey][label]:
+                    coords.append((valToIndx[x], valToIndy[y]))
+
+                matHeatAvg = sparse.COO(list(zip(*coords)), data=dicToPlotAvg[redKey][label]).todense()
+
+                sns.heatmap(matHeatAvg, cmap="afmhot_r", linewidth=0.03, vmin=0, vmax=1)
+
+                plt.xlabel(names[whatToPlot[1]])
+                plt.ylabel(names[whatToPlot[0]])
+                plt.gca().set_xticklabels([int(v) for i, v in sorted(valToIndy_inv.items())])
+                plt.gca().set_yticklabels([int(v) for i, v in sorted(valToIndx_inv.items())])
+                plt.title(str(list(zip(names, redKey.split("_")))))
+                plt.gca().invert_yaxis()
+                plt.tight_layout()
+                nameFig = ""
+                if whatToPlot == [4, 5]:
+                    nameFig = "ClusvsInterp"
+                savefig(nameFig, codeSave=redKey)
+                plt.close()
+                
 
 try:
     folder = sys.argv[1]
@@ -234,6 +368,7 @@ except Exception as e:
 list_params = [(features, output, DS, nbInterp, nbClus, buildData, seuil, folds)]
 
 plotRes()
+
 for features, output, DS, nbInterp, nbClus, buildData, seuil, folds in list_params:
     tabDicResAvg = []
     for fold in range(folds):
@@ -243,10 +378,7 @@ for features, output, DS, nbInterp, nbClus, buildData, seuil, folds in list_para
                 featToClus.append(iter)
         featToClus = np.array(featToClus, dtype=int)
 
-        codeSave = ""
-        for i in featToClus:
-            codeSave += f"{features[i]}({nbClus[i]})-"
-        codeSave += f"{output}" + "_fold-" + str(fold) + "of" + str(folds)+"_"
+        codeSave = f"{fold}of{folds}_{features}_{output}_{DS}_{nbInterp}_{nbClus}_"
 
         run = -1
         thetas, p, intToOut, intToFeat = read_params(folder, features, output, featToClus, nbClus, fold, run=run)
