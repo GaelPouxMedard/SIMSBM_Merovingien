0dc80058dba5274cdcc657e0def6feeb4fe87349
GaelPouxMedard <61467946+GaelPouxMedard@users.noreply.github.com>
2022-01-11 15:15:40 +0100

    Changes

--- run_XP.py
+++ run_XP.py
@@ -27,10 +27,8 @@ if __name__ == "__main__":
                 list_params.append((features, output, DS, nbInterp, nbClus, buildData, seuil, folds))
 
     with multiprocessing.Pool(processes=15) as p:
-        args = ()
         with tqdm.tqdm(total=len(list_params)) as progress:
-            args = [(folder, DS, features, output, nbInterp, nbClus, buildData, seuil, lim, propTrainingSet, folds, prec, nbRuns, maxCnt)
-                    for features, output, DS, nbInterp, nbClus, buildData, seuil, folds in list_params]
+            args = [(folder, DS, features, output, nbInterp, nbClus, buildData, seuil, lim, propTrainingSet, folds, prec, nbRuns, maxCnt) for features, output, DS, nbInterp, nbClus, buildData, seuil, folds in list_params]
             for i, res in enumerate(p.imap(runForOneDS, args)):
                 progress.update()
 
