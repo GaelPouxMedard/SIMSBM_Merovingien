ff6a642aa96223abf1c9d96806bf9d7c922184f7
GaelPouxMedard <gael.poux-medard@univ-lyon2.fr>
2022-09-21 16:45:37 +0200

    Updates...

--- run_all.py
+++ run_all.py
@@ -11,7 +11,7 @@ folds = 10
 nbRuns = 20
 list_output = [1, 2]
 list_nbInterp = [[1], [2], [3]]
-list_nbClus = [[3], [4], [5], [6], [7], [8], [9], [10]]
+list_nbClus = [[2], [3], [4], [5], [6], [7], [8], [9], [10]]
 
 prec = 1e-5  # Stopping threshold : when relative variation of the likelihood over 10 steps is < to prec
 maxCnt = 30  # Number of consecutive times the relative variation is lesser than prec for the algorithm to stop
