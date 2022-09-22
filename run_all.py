d2dfeff0b1276e577d78632ee30cc5eb98a715ae
GaelPouxMedard <gael.poux-medard@univ-lyon2.fr>
2022-09-22 15:36:55 +0200

    Updates...

--- run_all.py
+++ run_all.py
@@ -11,7 +11,7 @@ DS = [3]
 folds = 10
 nbRuns = 20
 list_output = [1, 2]
-#list_nbInterp = [[1], [2], [3]]
+list_nbInterp = [[1], [2], [3]]
 list_nbClus = [[2], [3], [4], [5], [6], [7], [8], [9], [10]]
 
 prec = 1e-5  # Stopping threshold : when relative variation of the likelihood over 10 steps is < to prec
@@ -30,18 +30,29 @@ buildData = False  # Build only once here
 list_nbInterp = [[1]]
 if __name__ == "__main__":
     os.system("python run_XP.py")  # For multiprocessing
+
+list_nbInterp = [[1]]
+list_nbClus = [[2], [3], [4], [5], [6], [7], [8], [9], [10]]
+if __name__ == "__main__":
     evaluate_all()
     visualize_all()
 
 list_nbInterp = [[2]]
 if __name__ == "__main__":
     os.system("python run_XP.py")  # For multiprocessing
+
+list_nbInterp = [[1], [2]]
+list_nbClus = [[2], [3], [4], [5], [6], [7], [8], [9], [10]]
+if __name__ == "__main__":
     evaluate_all()
     visualize_all()
 
 list_nbInterp = [[3]]
 if __name__ == "__main__":
-    list_nbClus = [[2], [3], [4], [5], [6], [7], [8], [9]]
     os.system("python run_XP.py")  # For multiprocessing
+
+list_nbInterp = [[1], [2], [3]]
+list_nbClus = [[2], [3], [4], [5], [6], [7], [8], [9], [10]]
+if __name__ == "__main__":
     evaluate_all()
     visualize_all()
